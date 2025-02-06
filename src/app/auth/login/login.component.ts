65f0427083f91760a9d5fef3bb4f067d50797a3a
selorm <kumiselorm@live.com>
2025-02-06 00:34:20 -0500

    login work

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
-
+import { Component } from "@angular/core";
+import { Router } from "@angular/router";
 @Component({
-  selector: 'app-login',
-  imports: [],
-  templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+	selector: "app-login",
+	imports: [],
+	templateUrl: "./login.component.html",
+	styleUrl: "./login.component.scss",
 })
 export class LoginComponent {
-
+	constructor(private router: Router) {}
+	onLogin() {
+		alert("Login successful!");
+		this.router.navigate(["/home"]); // redirect to home page is not working
+	}
 }
