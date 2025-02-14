383b14e45df3a71cd5c0603a5a46e0283340df21
selorm <kumiselorm@live.com>
2025-02-14 16:16:41 -0500

    dashboard nav working

--- src/app/patient/dashboard/dashboard.component.ts
+++ src/app/patient/dashboard/dashboard.component.ts
@@ -1,11 +1,9 @@
-import { Component } from '@angular/core';
-
+import { Component } from "@angular/core";
+import { RouterModule } from "@angular/router";
 @Component({
-  selector: 'app-dashboard',
-  imports: [],
-  templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss'
+	selector: "app-dashboard",
+	imports: [RouterModule],
+	templateUrl: "./dashboard.component.html",
+	styleUrl: "./dashboard.component.scss",
 })
-export class DashboardComponent {
-
-}
+export class DashboardComponent {}
