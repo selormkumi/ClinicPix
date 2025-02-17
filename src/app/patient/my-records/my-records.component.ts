ec98996c689c10d82f2101001d4f4cf30de494e1
selorm <kumiselorm@live.com>
2025-02-17 10:31:09 -0500

    logout function

--- src/app/patient/my-records/my-records.component.ts
+++ src/app/patient/my-records/my-records.component.ts
@@ -1,14 +1,16 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { Router, RouterModule } from "@angular/router";
 import { AuthenticationService } from "../../shared/services/authentication.service";
+
 @Component({
 	selector: "app-my-records",
 	imports: [RouterModule],
 	templateUrl: "./my-records.component.html",
 	styleUrl: "./my-records.component.scss",
 })
-export class MyRecordsComponent {
+export class MyRecordsComponent implements OnInit {
 	currentUserFullName: string | null = null;
+
 	constructor(
 		private authService: AuthenticationService,
 		private router: Router
@@ -22,6 +24,9 @@ export class MyRecordsComponent {
 		}
 	}
 
+	/**
+	 * The `logout` function logs the user out and redirects them to the login page.
+	 */
 	logout() {
 		this.authService.logout();
 		this.router.navigate(["/login"]); // Redirect to login after logout
