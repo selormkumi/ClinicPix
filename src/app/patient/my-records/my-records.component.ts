dc9b3130ca43afa9d192cfe148116eff03fe09de
selorm <kumiselorm@live.com>
2025-02-17 10:15:20 -0500

    minor update

--- src/app/patient/my-records/my-records.component.ts
+++ src/app/patient/my-records/my-records.component.ts
@@ -1,9 +1,33 @@
 import { Component } from "@angular/core";
-import { RouterModule } from "@angular/router";
+import { Router, RouterModule } from "@angular/router";
+import { AuthenticationService } from "../../shared/services/authentication.service";
 @Component({
 	selector: "app-my-records",
 	imports: [RouterModule],
 	templateUrl: "./my-records.component.html",
 	styleUrl: "./my-records.component.scss",
 })
-export class MyRecordsComponent {}
+export class MyRecordsComponent implements OnInit {
+	currentUserFullName: string | null = null;
+
+	constructor(
+		private authService: AuthenticationService,
+		private router: Router
+	) {}
+
+	ngOnInit() {
+		const currentUser = localStorage.getItem("user");
+		if (currentUser) {
+			const user = JSON.parse(currentUser);
+			this.currentUserFullName = user.fullName;
+		}
+	}
+
+	/**
+	 * The `logout` function logs the user out and redirects them to the login page.
+	 */
+	logout() {
+		this.authService.logout();
+		this.router.navigate(["/login"]); // Redirect to login after logout
+	}
+}
