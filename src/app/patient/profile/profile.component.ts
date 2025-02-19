be99b8a91b336528cf1cf37bf70118ed00300df9
selorm_kumi <kumiselorm@live.com>
2025-02-18 21:29:42 -0500

    logout is working for all pages

--- src/app/patient/profile/profile.component.ts
+++ src/app/patient/profile/profile.component.ts
@@ -1,9 +1,30 @@
-import { Component } from "@angular/core";
-import { RouterModule } from "@angular/router";
+import { Component, OnInit } from "@angular/core";
+import { RouterModule, Router } from "@angular/router";
+import { AuthenticationService } from "../../shared/services/authentication.service";
+
 @Component({
 	selector: "app-profile",
 	imports: [RouterModule],
 	templateUrl: "./profile.component.html",
 	styleUrl: "./profile.component.scss",
 })
-export class ProfileComponent {}
+export class ProfileComponent implements OnInit {
+	currentUserFullName: string | null = null;
+	constructor(
+		private authService: AuthenticationService,
+		private router: Router
+	) {}
+
+	ngOnInit() {
+		const currentUser = localStorage.getItem("user");
+		if (currentUser) {
+			const user = JSON.parse(currentUser);
+			this.currentUserFullName = user.fullName;
+		}
+	}
+
+	logout() {
+		this.authService.logout();
+		this.router.navigate(["/login"]); // Redirect to login after logout
+	}
+}
