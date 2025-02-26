e39138434bd077938ab00c5717dd7a6cb6c122ca
selorm_kumi <kumiselorm@live.com>
2025-02-25 20:15:28 -0500

    navigation for provider dashboard done

--- src/app/provider/dashboard/dashboard.component.ts
+++ src/app/provider/dashboard/dashboard.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
-
+import { Component } from "@angular/core";
+import { Router, RouterModule } from "@angular/router";
+import { AuthenticationService } from "../../shared/services/authentication.service";
 @Component({
-  selector: 'app-dashboard',
-  imports: [],
-  templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss'
+	selector: "app-dashboard",
+	imports: [RouterModule],
+	templateUrl: "./dashboard.component.html",
+	styleUrl: "./dashboard.component.scss",
 })
 export class DashboardComponent {
-
+	currentUserFullName: string | null = null;
+	constructor(
+		private authService: AuthenticationService,
+		private router: Router
+	) {}
+	ngOnInit() {
+		const currentUser = localStorage.getItem("user");
+		if (currentUser) {
+			const user = JSON.parse(currentUser);
+			this.currentUserFullName = user.fullName;
+		}
+	}
+	logout() {
+		this.authService.logout();
+		this.router.navigate(["/login"]); // Redirect to login after logout
+	}
 }
