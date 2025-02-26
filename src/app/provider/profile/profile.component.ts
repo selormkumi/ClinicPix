7a3785d842c3933ec74318424012c10c87cc0ed1
selorm_kumi <kumiselorm@live.com>
2025-02-25 21:09:51 -0500

    added basic function to provider

--- src/app/provider/profile/profile.component.ts
+++ src/app/provider/profile/profile.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
-
+import { Component } from "@angular/core";
+import { Router, RouterModule } from "@angular/router";
+import { AuthenticationService } from "../../shared/services/authentication.service";
 @Component({
-  selector: 'app-profile',
-  imports: [],
-  templateUrl: './profile.component.html',
-  styleUrl: './profile.component.scss'
+	selector: "app-profile",
+	imports: [RouterModule],
+	templateUrl: "./profile.component.html",
+	styleUrl: "./profile.component.scss",
 })
 export class ProfileComponent {
-
+	currentUserFullName: string | null = null;
+	constructor(
+		private authService: AuthenticationService,
+		private router: Router
+	) {}
+	ngOnInit() {
+		const currentUser = localStorage.getItem("user");
+		if (currentUser) {
+			const user = JSON.parse(currentUser);
+			this.currentUserFullName = user.fullName;
+		}
+	}
+	logout() {
+		this.authService.logout();
+		this.router.navigate(["/login"]); // Redirect to login after logout
+	}
 }
--- src/app/provider/uploaded-images/uploaded-images.component.ts
+++ src/app/provider/uploaded-images/uploaded-images.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
-
+import { Component } from "@angular/core";
+import { Router, RouterModule } from "@angular/router";
+import { AuthenticationService } from "../../shared/services/authentication.service";
 @Component({
-  selector: 'app-uploaded-images',
-  imports: [],
-  templateUrl: './uploaded-images.component.html',
-  styleUrl: './uploaded-images.component.scss'
+	selector: "app-uploaded-images",
+	imports: [RouterModule],
+	templateUrl: "./uploaded-images.component.html",
+	styleUrl: "./uploaded-images.component.scss",
 })
 export class UploadedImagesComponent {
-
+	currentUserFullName: string | null = null;
+	constructor(
+		private authService: AuthenticationService,
+		private router: Router
+	) {}
+	ngOnInit() {
+		const currentUser = localStorage.getItem("user");
+		if (currentUser) {
+			const user = JSON.parse(currentUser);
+			this.currentUserFullName = user.fullName;
+		}
+	}
+	logout() {
+		this.authService.logout();
+		this.router.navigate(["/login"]); // Redirect to login after logout
+	}
 }
