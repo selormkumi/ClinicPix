d8a515aeb689f355c0edca63a60aa236065c814c
selorm <kumiselorm@live.com>
2025-03-11 14:59:49 -0400

    added search for shared images too

--- src/app/provider/shared-images/shared-images.component.ts
+++ src/app/provider/shared-images/shared-images.component.ts
@@ -12,6 +12,10 @@ import { ImageModalComponent } from "../../shared/image-modal/image-modal.compon
 	styleUrl: "./shared-images.component.scss",
 })
 export class SharedImagesComponent {
+	searchTerm: string = ""; // Holds the search term entered by the user
+	filteredImages: any[] = []; // Array that holds the filtered shared images
+	selectedImage: any = null; // Holds the selected image for the modal
+
 	sharedImages = [
 		{
 			name: "CT_Scan_2025.jpg",
@@ -27,14 +31,32 @@ export class SharedImagesComponent {
 		},
 	];
 
-	selectedImage: any | null = null;
-
 	constructor(
 		private authService: AuthenticationService,
 		private router: Router
-	) {}
+	) {
+		this.filteredImages = this.sharedImages;
+	}
 	ngOnInit() {}
-
+	onSearch() {
+		if (this.searchTerm.trim() === "") {
+			this.filteredImages = this.sharedImages; // Show all images if the search term is empty
+		} else {
+			this.filteredImages = this.sharedImages.filter(
+				(image) =>
+					image.name.toLowerCase().includes(this.searchTerm.toLowerCase()) ||
+					image.sharedBy
+						.toLowerCase()
+						.includes(this.searchTerm.toLowerCase()) ||
+					image.sharedOn
+						.toLowerCase()
+						.includes(this.searchTerm.toLowerCase()) ||
+					image.tags.some((tag) =>
+						tag.toLowerCase().includes(this.searchTerm.toLowerCase())
+					)
+			);
+		}
+	}
 	viewImage(image: any) {
 		this.selectedImage = image;
 	}
