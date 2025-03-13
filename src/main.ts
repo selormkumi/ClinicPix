6e1b4aa64d0cbaf59d56bb62ff051a5107d07ca0
Tan Zhi Ern <zhiern.tan96@gmail.com>
2025-03-13 15:41:07 -0400

    starting fresh

--- src/main.ts
+++ src/main.ts
@@ -5,17 +5,6 @@ import { routes } from "./app/app.routes";
 import { provideHttpClient, withInterceptors } from "@angular/common/http";
 import { provideAnimationsAsync } from "@angular/platform-browser/animations/async";
 import { AuthInterceptor } from "./app/shared/interceptors/auth.interceptor";
-import { Amplify } from "aws-amplify";
-import { awsconfig } from "./aws-exports";
-
-Amplify.configure({
-	Auth: {
-		Cognito: {
-			userPoolId: "ap-south-1_xxxxx",
-			userPoolClientId: "71h7gnxxxxxxxxx",
-		},
-	},
-});
 
 bootstrapApplication(AppComponent, {
 	providers: [
