e1fb2498d3bef82cf6ccf3ef20d376f171c1caf3
Tan Zhi Ern <zhiern.tan96@gmail.com>
2025-03-21 02:53:03 -0400

    incorporated RDS

--- serverside/config/dbConfig.js
+++ serverside/config/dbConfig.js
@@ -1,16 +1,22 @@
 const { Pool } = require("pg");
-require("dotenv").config(); // Ensure environment variables are loaded
+require("dotenv").config(); // Load environment variables
+const fs = require("fs");
+const path = require("path");
 
 const db = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "clinicpix",
-  password: "447652$#$",
-  port: 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: true,
+    ca: fs.readFileSync(path.join(__dirname, "../certificates/us-east-2-bundle.pem")).toString(),
+  },
 });
 
 db.connect()
-    .then(() => console.log("✅ Connected to PostgreSQL Database"))
-    .catch((err) => console.error("❌ Database Connection Error:", err));
+  .then(() => console.log(`✅ Connected to AWS RDS PostgreSQL Database`))
+  .catch((err) => console.error("❌ Database Connection Error:", err));
 
 module.exports = db;
