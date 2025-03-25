9a8ddd784aed7482d29539835745d51d4f790d70
selorm <kumiselorm@live.com>
2025-03-25 19:47:53 -0400

    changes to file structure

--- src/app/admin/admin.routes.ts
+++ src/app/admin/admin.routes.ts
@@ -1,7 +1,6 @@
 import { Routes } from "@angular/router";
 import { HomeComponent } from "./home/home.component";
 import { ManageUsersComponent } from "./manage-users/manage-users.component";
-import { ManageImagesComponent } from "./manage-images/manage-images.component";
 import { AuditComponent } from "./audit/audit.component";
 import { AuthGuard } from "../shared/guards/auth.guard";
 
@@ -12,10 +11,6 @@ export const ADMIN_ROUTES: Routes = [
 		component: ManageUsersComponent,
 		canActivate: [AuthGuard],
 	},
-	{
-		path: "manage-images",
-		component: ManageImagesComponent,
-		canActivate: [AuthGuard],
-	},
+
 	{ path: "audit", component: AuditComponent, canActivate: [AuthGuard] },
 ];
